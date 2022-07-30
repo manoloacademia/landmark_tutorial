7db49af2d99843b3f5bc25596d35c82998b64084
manoloacademia <luispablosegovia@icloud.com>
2022-07-30 09:21:56 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Pablo Segovia on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
